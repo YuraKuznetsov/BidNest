5c3431e5d59a9f25ae473acd9c697b8901b29e8b
Yurii Kuznetsov <yurakuznetsov23@gmail.com>
2023-10-21 17:38:21 +0300

    Create BidNestApplication

---
+++ src/main/java/com/bidnest/BidNestApplication.java
@@ -0,0 +1,12 @@
+package com.bidnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidNestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BidNestApplication.class, args);
+    }
+}
