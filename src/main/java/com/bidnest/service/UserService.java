65cafe7ae471ee4412fe20de0fd8c7105b6b6016
Yurii Kuznetsov <yurakuznetsov23@gmail.com>
2023-10-24 20:39:31 +0300

    Add method getCurrentUser()

--- src/main/java/com/bidnest/service/UserService.java
+++ src/main/java/com/bidnest/service/UserService.java
@@ -1,7 +1,10 @@
 package com.bidnest.service;
 
+import com.bidnest.exception.ItemNotFoundException;
+import com.bidnest.model.user.User;
 import com.bidnest.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,5 +13,10 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    public User getCurrentUser() {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
 
+        return userRepository.findUserByEmail(email)
+                .orElseThrow(() -> new ItemNotFoundException("User not found"));
+    }
 }
